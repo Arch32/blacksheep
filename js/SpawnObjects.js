cc206856afdeca03e5427dfec14eb882faf3bdaf
Archana <ab.archana28@gmail.com>
2021-03-06 11:34:48 +0530

    code

--- js/SpawnObjects.js
+++ js/SpawnObjects.js
@@ -1,9 +1,36 @@
 function  spawnBlackSheep(){
-    if (frameCount % 200 === 0  ){
+    if (frameCount % 300 === 0  ){
         var blackSheep  = createSprite(displayWidth, displayHeight-100, 40,20);
         blackSheep.addImage(blackSheepImg);
         blackSheep.scale = 0.8;
-        blackSheep.velocityX = -3;
+        blackSheep.velocityX = -4;
         blackSheep.lifeTime = 200;
+        blackSheepsGroup.add(blackSheep);
+        count = count+1;
     }
+}
+
+function shootTheSheep(){
+    console.log("*************");
+    var bomb = createSprite(displayWidth/4+50,displayHeight-75, 10,10);
+    bomb.y = raftaar.raftaarSprite.y;
+    bomb.velocityX = 3;  
+    bomb.velocityY = random(0,5); 
+    console.log(bomb.velocityY);
+    bombsGroup.add(bomb); 
+}
+
+function destroySheep(){
+    if(count > 0){
+        for(var i=0; i<count; i++){
+          //console.log(blackSheepsGroup.get(i));
+          if(blackSheepsGroup.get(i) !== undefined  ){
+            if(bombsGroup.isTouching(blackSheepsGroup.get(i))){
+              blackSheepsGroup.get(i).destroy();
+              bombsGroup.destroyEach();             
+            }
+          }         
+        }
+      }
+  
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 var raftaar, invisibleGround;
 var blackSheepImg, raftaarImg;
+var bombsGroup, blackSheepsGroup;
+var score = 0;
+var count = 0;
 
 function preload(){
   raftaarImg =  loadImage("images/ninja.gif");
@@ -9,8 +12,10 @@ function preload(){
 function setup(){
   createCanvas(displayWidth-20, displayHeight-20);
   invisibleGround =  createSprite(displayWidth/2, displayHeight-20,displayWidth,20);
+  invisibleGround.visible = false;
   raftaar = new Raftaar();
-
+  bombsGroup = new Group();
+  blackSheepsGroup = new Group();
 }
 
 
@@ -19,5 +24,17 @@ function draw(){
   raftaar.jump();
   spawnBlackSheep();
   raftaar.raftaarSprite.collide(invisibleGround);
+  destroySheep();
+  
+     
+
+  
+ 
   drawSprites();
+}
+  
+function keyPressed(){
+  if(keyCode === 66){
+    shootTheSheep();
+  }
 }
